1ec470627fe78c307bbdd705540a8f7bb957646c
john <john.b.gavin+git@gmail.com>
2014-11-24 22:47:06 +0000

    ..
    …

---
+++ h2o/01_rh2o.R
@@ -0,0 +1,25 @@
+# TODO: Add comment
+# 
+# Author: jbg
+###############################################################################
+
+
+
+# The following two commands remove any previously installed H2O packages for R.
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+# Next, we download, install and initialize the H2O package for R.
+install.packages("h2o", repos=(c("http://s3.amazonaws.com/h2o-release/h2o/rel-kahan/5/R", getOption("repos"))))
+
+library(h2o)
+localH2O = h2o.init()
+
+# Finally, let's run a demo to see H2O at work.
+demo(h2o.glm)
+
+
+
+library(devtools)
+devtools::source_gist(11049319)
+
